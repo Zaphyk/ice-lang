d0531f4591e01bb181ebb2ca0dd40354d2f4c5fa
Zaphyk <zaphyk@live.com>
2017-09-24 22:29:48 -0300

    building up directory

---
+++ interpreter/interpreter.cpp
@@ -0,0 +1,15 @@
+// interpreter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	cout << "asd";
+	cin.get();
+    return 0;
+}
+
